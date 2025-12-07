0798f4aa94f79963945fb16cfba74707837b66b2
prnth <pranith_h@yahoo.com.sg>
2025-12-07 12:02:58 +0530

    types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
+// Components
 export { default as GameCanvas } from './shared/GameCanvas';
 export { default as PrefabEditor } from './tools/prefabeditor/PrefabEditor';
 export { default as PrefabRoot } from './tools/prefabeditor/PrefabRoot';
 export { DragDropLoader } from './tools/dragdrop/DragDropLoader';
-export { default as AssetViewerPage, TextureListViewer, ModelListViewer, SoundListViewer, SharedCanvas } from './tools/assetviewer/page';
+export {
+  default as AssetViewerPage,
+  TextureListViewer,
+  ModelListViewer,
+  SoundListViewer,
+  SharedCanvas,
+} from './tools/assetviewer/page';
+
+// Types
+export type { Prefab, GameObject } from './tools/prefabeditor/types';

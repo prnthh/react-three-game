1620806a154db49f2b8a86830eee71bd30730e86
prnth <pranith_h@yahoo.com.sg>
2025-12-07 11:09:10 +0530

    Update next.config.ts

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  // fix because this is on prnth.com/react-three-game
+  basePath: '/react-three-game',
   images: {
     unoptimized: true,
   },
